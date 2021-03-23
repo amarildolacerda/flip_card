6ca68b93e589ab816378e6c07d16b94a7d1ce045
amarildolacerAda <amarildo_51@msn.com>
2021-03-23 13:20:24 -0300

    .

--- lib/flip_card.dart
+++ lib/flip_card.dart
@@ -1,3 +1,5 @@
+// @dart=2.12
+
 library flip_card;
 
 import 'dart:math';
@@ -11,21 +13,21 @@ enum FlipDirection {
 class AnimationCard extends StatelessWidget {
   AnimationCard({this.child, this.animation, this.direction});
 
-  final Widget child;
-  final Animation<double> animation;
-  final FlipDirection direction;
+  final Widget? child;
+  final Animation<double>? animation;
+  final FlipDirection? direction;
 
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-      animation: animation,
-      builder: (BuildContext context, Widget child) {
+      animation: animation!,
+      builder: (BuildContext context, Widget? child) {
         var transform = Matrix4.identity();
         transform.setEntry(3, 2, 0.001);
         if (direction == FlipDirection.VERTICAL) {
-          transform.rotateX(animation.value);
+          transform.rotateX(animation!.value);
         } else {
-          transform.rotateY(animation.value);
+          transform.rotateY(animation!.value);
         }
         return Transform(
           transform: transform,
@@ -47,8 +49,8 @@ class FlipCard extends StatefulWidget {
   /// The amount of milliseconds a turn animation will take.
   final int speed;
   final FlipDirection direction;
-  final VoidCallback onFlip;
-  final BoolCallback onFlipDone;
+  final VoidCallback? onFlip;
+  final BoolCallback? onFlipDone;
 
   /// When enabled, the card will flip automatically when touched. This behavior
   /// can be disabled if this is not desired. To manually flip a card from your
@@ -76,9 +78,9 @@ class FlipCard extends StatefulWidget {
   final bool flipOnTouch;
 
   const FlipCard(
-      {Key key,
-      @required this.front,
-      @required this.back,
+      {Key? key,
+      required this.front,
+      required this.back,
       this.speed = 500,
       this.onFlip,
       this.onFlipDone,
@@ -94,9 +96,9 @@ class FlipCard extends StatefulWidget {
 
 class FlipCardState extends State<FlipCard>
     with SingleTickerProviderStateMixin {
-  AnimationController controller;
-  Animation<double> _frontRotation;
-  Animation<double> _backRotation;
+  late AnimationController controller;
+  Animation<double>? _frontRotation;
+  Animation<double>? _backRotation;
 
   bool isFront = true;
 
@@ -134,14 +136,14 @@ class FlipCardState extends State<FlipCard>
     controller.addStatusListener((status) {
       if (status == AnimationStatus.completed ||
           status == AnimationStatus.dismissed) {
-        if (widget.onFlipDone != null) widget.onFlipDone(isFront);
+        if (widget.onFlipDone != null) widget.onFlipDone!(isFront);
       }
     });
   }
 
   void toggleCard() {
     if (widget.onFlip != null) {
-      widget.onFlip();
+      widget.onFlip!();
     }
     if (isFront) {
       controller.forward();
@@ -175,7 +177,7 @@ class FlipCardState extends State<FlipCard>
     return child;
   }
 
-  Widget _buildContent({@required bool front}) {
+  Widget _buildContent({required bool front}) {
     // pointer events that would reach the backside of the card should be
     // ignored
     return IgnorePointer(
